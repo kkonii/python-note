4441f95609c3e4e4fcdf69e6338a89e4e3d4fb59
kkonii <119216472+kkonii@users.noreply.github.com>
2023-08-21 18:10:44 +0000

    1주차 - print 기본 문법/문자열 결합

---
+++ week1/print.py
@@ -0,0 +1,11 @@
+#기본 문자 출력
+print("Hello World!")
+
+#""안에 "" 불가 -> ''로
+print("HEllow 'MR'!")
+
+#\n = enter
+print("Hello World!\nHello World!")
+
+#문자열 결합 :  서로 다른 문자열을 합침
+print("Hello " + "jinnie")
