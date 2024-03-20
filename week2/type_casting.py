7e2284298443b2d249fe53f2fc96c9c97db26e62
kkoni <119216472+kkonii@users.noreply.github.com>
2024-03-20 14:57:26 +0000

    형변환 함수 사용법

---
+++ week2/type_casting.py
@@ -0,0 +1,21 @@
+#---------------형변환-----------------------
+
+num_char = len(input("What is your name?"))
+
+#str( ) - 문자열 타입으로 형변환 해주는 함수
+casting_num_char = str(num_char)
+
+print("Your name has " + casting_num_char + " characters.")
+#결과값 : Your name has {number} characters.
+
+#------------- another type casting ----------------
+#int
+a = 123
+
+#String
+#"123"
+aa = str(123)
+
+#Float
+#123.0
+b = float(123)
